82428aa92a5146358267fb7fcf92d001a477ad64
Arrettino <batukearrettino@outlook.com>
2021-02-11 14:55:58 -0300

    create basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component'
+import { ExpensesComponent } from './pages/expenses/expenses.component'
+import {NotFoundComponent} from './pages/not-found/not-found.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'expenses', component: ExpensesComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExpensesComponent } from './pages/expenses/expenses.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExpensesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
